e7c955770be37c49213ce737118ec341119f62a7
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2021-10-15 19:57:53 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ fi
 
 tar -xzvf $template_file_dir/facebooc-bin.tar.gz -C $bin_exec_dir/
 
-cd $bin_exec_dir/facebooc && ./bin/facebooc
+cd $bin_exec_dir/facebooc && ./bin/facebooc $(PORT)
 
 #if [ $(pwd) = $bin_exec_dir ] && [ -e "$bin_exec_dir" ]; then
 #	./bin/facebooc
