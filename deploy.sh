96c19ded49aaea7ff8f47cc9f236795be619d472
eqpoqpe <eqpoqpe@gmail.com>
2021-10-15 09:41:12 +0800

    None

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Deploy for facebooc [https://github.com/jsver/facebooc]"
-cd ~
+
+cd /root
 
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/eqpoqpe/facebooc-deploy/releases/download/v1.0.0/facebooc-v1.0.0-amd64_linux.tar.gz
 
-tar -xzf facebooc-v1.0.0-amd64_linux.tar.gz
-cd facebooc
+tar -xzf ./facebooc-v1.0.0-amd64_linux.tar.gz
+
+cd ./facebooc
+
 ./bin/facebooc
